db965864c963f4ca6773d5ccc08c64576122e77f
steves3142 <helixdeveloper1@gmail.com>
2024-12-30 22:00:14 -0500

    pre sanity build

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -12,7 +12,7 @@ import ContactMe from '@/components/ContactMe';
 
 const Home = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#0af7bc]/80">
       <Head >
         <title className="text-black">Steve's Portfolio</title>
       </Head>
