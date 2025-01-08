f32ff0cb43bb30f76cbf880a3c75bdf8338fd980
steves3142 <helixdeveloper1@gmail.com>
2025-01-08 09:35:07 -0500

    adding github icon

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa"; // Import FaGithub
+import { FaGithub } from "react-icons/fa"; 
 import Link from 'next/link';
 import { MdOutlineMailOutline } from "react-icons/md";
-import { fetchSocial } from '../utils/fetchSocials'; // Import the fetchSocial function
+// import { fetchSocial } from '../utils/fetchSocials'; 
 
 // Define a mapping for the icons with the correct type
-const iconMapping: { [key: string]: React.ComponentType<React.SVGProps<SVGSVGElement>> } = {
-    X: FaXTwitter,          // Map X to FaXTwitter
-    LinkedIn: FaLinkedin,   // Map LinkedIn to FaLinkedin
-    GitHub: FaGithub,       // Map GitHub to FaGithub
-    // Add more mappings here if necessary
-};
+// const iconMapping: { [key: string]: React.ComponentType<React.SVGProps<SVGSVGElement>> } = {
+//     X: FaXTwitter,          // Map X to FaXTwitter
+//     LinkedIn: FaLinkedin,   // Map LinkedIn to FaLinkedin
+//     GitHub: FaGithub,       // Map GitHub to FaGithub
+//     // Add more mappings here if necessary
+// };
 
 export default function Header() {
-    const [socials, setSocials] = useState<{ title: string, url: string }[]>([]); // State to store fetched socials data
+    // const [socials, setSocials] = useState<{ title: string, url: string }[]>([]); // State to store fetched socials data
 
     // Fetch the socials data when the component mounts
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const fetchedSocials = await fetchSocial(); // Fetch socials using the fetchSocial function
-                setSocials(fetchedSocials); // Update state with fetched data
-            } catch (error) {
-                console.error('Error fetching socials data:', error);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const fetchedSocials = await fetchSocial(); // Fetch socials using the fetchSocial function
+    //             setSocials(fetchedSocials); // Update state with fetched data
+    //         } catch (error) {
+    //             console.error('Error fetching socials data:', error);
+    //         }
+    //     };
 
-        fetchData(); // Call the function to fetch data
-    }, []);
+    //     fetchData(); // Call the function to fetch data
+    // }, []);
 
     return (
         <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
             <div className="flex flex-row items-center">
-                {socials.map((social) => {
-                    // console.log(social.url)
-                    const Icon = iconMapping[social.title]; // Get the corresponding icon component from the title
-                    return (
-                        <a key={social.url} href={social.url} target="_blank" rel="noopener noreferrer">
-                            <Icon className="text-2xl" /> {/* Now, size can be passed correctly */}
-                        </a>
-                    );
-                })}
+                {/* Render Social Icons only on the client */}
+                <a href="https://x.com/Steves314" target="" rel="">
+                    <FaXTwitter size={25} />
+                </a>
+                <a href="https://www.linkedin.com/in/steve-susanibar" target="" rel="">
+                    <FaLinkedin size={25} />
+                </a>
+                <a href="https://github.com/steves3142" target="" rel="">
+                    <FaGithub size={25} />
+                </a>
             </div>
+
 
             <Link href="#contact">
                 <div className="flex flex-row items-center text-gray-300 cursor-pointer">
